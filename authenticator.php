ff2128f06cd4711a359094f031be533125696065
Alejandro Caballero <lava.caballero@gmail.com>
2021-02-27 00:08:55 -0600

    Added 2FA support

--- authenticator.php
+++ authenticator.php
@@ -5,9 +5,12 @@
  * @package mobile_controller
  * @author  Alejandro Caballero - lava.caballero@gmail.com
  * 
+ * @var module[] $modules
+ * 
  * @param string "username"
  * @param string "password" MD5 hash
  * @param string "device"
+ * @param string "tfa_code"
  * @param string "callback" Optional, for AJAX call
  * 
  * @returns string JSON {message:string, data:mixed}
@@ -15,6 +18,7 @@
 
 use hng2_base\account;
 use hng2_base\device;
+use hng2_base\module;
 use hng2_modules\mobile_controller\toolbox;
 
 include "../config.php";
@@ -22,6 +26,8 @@ include "../includes/bootstrap.inc";
 header("Content-Type: application/json; charset=utf-8");
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST');
+
+include_once ROOTPATH . "/accounts/2fa/totp.php";
 
 $toolbox = new toolbox();
 
@@ -43,6 +49,15 @@ if( empty($_REQUEST["device"]) )
 if( strlen($_REQUEST["password"]) != 32 )
     $toolbox->throw_response(trim($current_module->language->authenticator->invalid_password_provided));
 
+try
+{
+    check_sql_injection($_REQUEST["username"]);
+}
+catch(\Exception $e)
+{
+    $toolbox->throw_response($e->getMessage());
+}
+
 $account = new account($_REQUEST["username"]);
 
 if( ! $account->_exists )
@@ -54,10 +69,39 @@ if( $account->password != $_REQUEST["password"] )
 if( $account->state != "enabled" )
     $toolbox->throw_response(trim($current_module->language->authenticator->account_disabled));
 
+#
+# 2FA checkup
+#
+
+$device_string = stripslashes($_REQUEST["device"]);
+if( $settings->get("modules:accounts.use_2fa") == "true" && ! empty($account->engine_prefs["@accounts:2fa_secret"]) )
+{
+    if( ! preg_match("@bardcanvas mobile/([0-9.]+)@i", $device_string, $matches) )
+    {
+        $toolbox->throw_response(trim($current_module->language->authenticator->tfa_required->invalid_client));
+    }
+    else
+    {
+        $client_version = $matches[1];
+        if( version_compare($client_version, "1.1.2") < 0 )
+        {
+            $toolbox->throw_response(trim($current_module->language->authenticator->tfa_required->older_client));
+        }
+        
+        $tfa_code = trim(stripslashes($_REQUEST["tfa_code"]));
+        if( ! is_numeric($tfa_code) ) $toolbox->throw_response("@ASK_FOR_2FA");
+        
+        $enc_secret = $account->engine_prefs["@accounts:2fa_secret"];
+        $raw_secret = three_layer_decrypt($enc_secret, $config->website_key, $account->id_account, $account->creation_date);
+        $totp       = new totp();
+        $res        = $totp->verifyCode($raw_secret, $tfa_code);
+        if( ! $res ) $toolbox->throw_response(trim($current_module->language->authenticator->tfa_required->wrong_code));
+    }
+}
+
 $meta = array();
 $current_module->load_extensions("authenticator", "after_meta_init");
 
-$device_string = stripslashes($_REQUEST["device"]);
 $device_record = new device($account->id_account, $device_string);
 if( $device_record->_exists )
 {
